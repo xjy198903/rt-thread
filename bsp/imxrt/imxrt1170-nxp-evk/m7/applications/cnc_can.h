2a795a527e25dba3e8659b06cd171ba05db57b38
xjy <335266746@qq.com>
2022-11-14 17:56:33 +0800

    修正can宏定义

--- bsp/imxrt/imxrt1170-nxp-evk/m7/applications/cnc_can.h
+++ bsp/imxrt/imxrt1170-nxp-evk/m7/applications/cnc_can.h
@@ -2,8 +2,8 @@
 #define __CNC_CAN_H__
 
 #define CAN_THREAD_PRIORITY 12
-#define CAN_THREAD_TICK 5
-#define CAN_STACK_SIZE 1024
+#define CAN_THREAD_TICK 5U
+#define CAN_STACK_SIZE 1024U
 #define CAN_DEVICE_NAME "can2"
 
 void can_comm_entry(void*);
